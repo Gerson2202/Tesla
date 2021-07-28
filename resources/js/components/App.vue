d739a6539254eb1d307ea22354cffd3f953d860e
gerson mojica <gersonpsj@gmail.com>
2021-07-28 15:27:02 -0500

    first commit

---
+++ resources/js/components/App.vue
@@ -0,0 +1,51 @@
+
+<template>
+
+    <div>
+        <h1 style="margin-toh1:200px;text-align:center">Todo list</h1>
+        
+        <div class="todoListContainer">       
+        <add-item-form/>
+        
+
+        </div>
+    </div>
+    
+</template>
+
+
+<script>
+
+// componentes creados
+import AddItemForm from "../components/addItemForm.vue"
+import listItem from "../components/listItem.vue"
+import listView from "../components/listView.vue"
+
+
+export default {
+
+    // IMPORTAMOS LOS COMPONENTES QUE CREAMOS PARA PODER MOSTRARLOS ACA
+
+   components:{
+       AddItemForm,
+       listItem,
+       listView
+   },
+    
+    data(){
+        return {
+            
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+  .todoListContainer{
+    width: 400px;
+    margin: auto;
+    margin-top: 50px;
+  }
+
+</style>
---
+++ resources/js/components/addItemForm.vue
@@ -0,0 +1,60 @@
+<template>
+
+    <div class="addItem">
+        <input type="text" class="input" placeholder="add todo">
+        <button class="addButton">
+             <!-- <font-awesome-icon icon="user-secret" />        -->
+
+
+        </button>
+    </div>
+    
+    
+</template>
+
+
+<script>
+
+
+export default ({
+   
+})
+
+
+</script>
+
+<style>
+    .addItem{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-bottom: 25px;
+    }
+
+    .input{
+        border-style: solid;
+        border-top-color: blue;
+        border-bottom-left-radius: 4px;
+        border-top-left-radius: 4px;
+        outline: none;
+        padding: 5px;
+        height: 30px;
+        width: 100%;
+    }
+    .addButton{
+
+        height: 30px;
+        width: 40px;
+        border-style: solid;
+        border-color: blue;
+        border-bottom-right-radius: 4px;
+        border-top-right-radius: 4px;
+        border-width: 1px;
+        background-color: white;
+    }
+
+    .icon-plus{
+        font-size: 20px;
+        color: blue;
+    }
+</style>
